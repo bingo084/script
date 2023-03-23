ddac558177facbc3087e5c938fda0d2c3b0fbbd6
王斌 <bingo084@outlook.com>
2023-03-23 18:05:00 +0800

    feat: clash start.sh curl add timeout limit

--- clash/start-clash.sh
+++ clash/start-clash.sh
@@ -3,14 +3,14 @@
 # save pid file
 echo $$ > ${HOME}/.config/clash/clash.pid
 
-diff ${HOME}/.config/clash/config.yaml <(curl -s ${CLASH_URL})
+diff ${HOME}/.config/clash/config.yaml <(curl -s ${CLASH_URL} --connect-timeout 10 -m 20 --noproxy '*')
 if [ "$?" == 0 ]
 then
     /usr/bin/clash
 else
     TIME=`date '+%Y-%m-%d %H:%M:%S'`
     cp ${HOME}/.config/clash/config.yaml "${HOME}/.config/clash/config.yaml.bak${TIME}"
-    curl -L -o ${HOME}/.config/clash/config.yaml ${CLASH_URL}
     find ${HOME}/.config/clash -mmin +3 -name 'config.yaml.bak*' -exec rm -rf {} \;
+    curl -L -o ${HOME}/.config/clash/config.yaml ${CLASH_URL} --connect-timeout 10 -m 20 --noproxy '*'
     /usr/bin/clash
 fi
