b35593bc189fbfc675126443960842bffaec90b3
王斌 <bingo084@outlook.com>
2023-03-23 21:23:47 +0800

    feat: clash start.sh add config.yaml verify

--- clash/start-clash.sh
+++ clash/start-clash.sh
@@ -8,9 +8,15 @@ if [ "$?" == 0 ]
 then
     /usr/bin/clash
 else
-    TIME=`date '+%Y-%m-%d %H:%M:%S'`
-    cp ${HOME}/.config/clash/config.yaml "${HOME}/.config/clash/config.yaml.bak${TIME}"
-    find ${HOME}/.config/clash -mmin +3 -name 'config.yaml.bak*' -exec rm -rf {} \;
-    curl -L -o ${HOME}/.config/clash/config.yaml ${CLASH_URL} --connect-timeout 10 -m 20 --noproxy '*'
+    curl -L -o ${HOME}/.config/clash/config.new.yaml ${CLASH_URL} --connect-timeout 10 -m 20 --noproxy '*'
+    if [[ $(wc -l < ${HOME}/.config/clash/config.new.yaml) < 20 ]]
+    then
+        rm -rf ${HOME}/.config/clash/config.new.yaml
+    else
+        TIME=`date '+%Y-%m-%d %H:%M:%S'`
+        cp ${HOME}/.config/clash/config.yaml "${HOME}/.config/clash/config.yaml.bak${TIME}"
+        find ${HOME}/.config/clash -mtime +1 -name 'config.yaml.bak*' -exec rm -rf {} \;
+        mv {HOME}/.config/clash/config.new.yaml {HOME}/.config/clash/config.yaml
+    fi
     /usr/bin/clash
 fi
